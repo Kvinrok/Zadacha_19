cc64da1f05006a85554a08fad824ee605b73b7a3
Alexadr <sanz76@.mail.ru>
2022-12-08 14:26:22 +0300

    DZ_19

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("Введите 5-ти значное число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number [1] == number[3])
+    {
+        Console.WriteLine(number + " Палиндром");
+    }
+
+    else
+    {
+        Console.WriteLine(number + " Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine ("Вы ввели НЕ 5-и значное число");
+}
